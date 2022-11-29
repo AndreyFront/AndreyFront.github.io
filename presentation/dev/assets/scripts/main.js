863256ede595c131fcca5459332b1d9403b37c88
AndreyFront <andrey-se-v@yandex.ru>
2022-11-29 16:36:10 +0300

    made a desktop header + footer + performance section

---
+++ presentation/dev/assets/scripts/main.js
@@ -0,0 +1,51 @@
+window.onload = () => {
+    function page() {
+        const main = document.querySelector('[data-page="main"]')
+        const header = document.querySelector('[data-header="main"]')
+    
+        if (!main && !header ) return
+    
+        main.style.paddingTop = `${header.offsetHeight}px`
+    }
+
+    function fixedHeader() {     
+        const header = document.querySelector('[data-header="main"]')
+    
+        if (!header) return
+    
+        if (window.matchMedia("(min-width: 992px)").matches) {
+            window.addEventListener('scroll', (event) => {
+                const scrolled = window.pageYOffset ? window.pageYOffset : document.body.scrollTop;
+    
+                if (scrolled >= 20) {
+                    header.classList.add('scroll-active')
+                } else {
+                    header.classList.remove('scroll-active')
+                }
+            })
+        }
+    }
+
+    function smoothScrolling() {
+        const anchors = document.querySelectorAll('[data-smooth-scrolling*="#"]')
+    
+        if (!anchors.length) return
+    
+        anchors.forEach(anchor => {
+            anchor.addEventListener('click', (event) => {
+                event.preventDefault()
+                
+                const blockID = anchor.getAttribute('data-smooth-scrolling').substr(1)
+                
+                document.querySelector(`[data-smooth-scrolling="${blockID}"]`).scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                })
+            })
+        })
+    }
+
+    page()
+    fixedHeader()
+    smoothScrolling()
+}
