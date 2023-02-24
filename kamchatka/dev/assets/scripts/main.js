33a214be4f6bc23f9873367e3da64b4f2f12bcd6
AndreyFront <andrey-se-v@yandex.ru>
2023-02-24 17:58:17 +0300

    the entire main and part of the tour page has been made

---
+++ kamchatka/dev/assets/scripts/main.js
@@ -0,0 +1,40 @@
+function header() {
+    const header = document.querySelector('[data-header="main"]')
+
+    if (!header) return
+
+    const menu = header.querySelector('[data-header="menu"]')
+    const btnMenu = header.querySelector('[data-header="btn-menu"]')
+
+    window.addEventListener('scroll', (event) => {
+        const scrolled = window.pageYOffset ? window.pageYOffset : document.body.scrollTop;
+
+        if (scrolled >= 100) {
+            header.classList.add('header--fixed')
+        } else {
+            header.classList.remove('header--fixed')
+        }
+    })
+
+    if (window.matchMedia("(max-width: 992px)").matches) {
+        btnMenu.addEventListener('click', () => {
+            btnMenu.classList.toggle('active')
+            menu.classList.toggle('active')
+        })
+    }
+}
+
+function presentation() {
+    const main = document.querySelector('[data-presentation="main"]')
+
+    if (!main) return
+
+    const blockParallax = main.querySelector('[data-presentation="block-parallax"]')
+
+    const parallaxInstance = new Parallax(blockParallax, {
+        hoverOnly: true
+    })
+}
+
+header()
+presentation()
